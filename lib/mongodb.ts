e5d22aba80b5e8ffecd804d7a65931d1f54acd9e
alifaizan786-op <faizan.ali@irunadigital.com>
2026-04-22 01:51:00 -0400

    added mongodb connection in lib/mongodb.ts

---
+++ lib/mongodb.ts
@@ -0,0 +1,22 @@
+// lib/mongodb.ts
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+// Point cached directly at global.mongoose so any updates to cached.conn
+// automatically update global.mongoose.conn too
+global.mongoose = global.mongoose || { conn: null, promise: null };
+let cached = global.mongoose;
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
