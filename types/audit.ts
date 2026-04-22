9b44a13d989662b54141ce2e548619f91c18d29e
alifaizan786-op <faizan.ali@irunadigital.com>
2026-04-22 01:37:16 -0400

    created type for AuditResult in types/audit.ts

---
+++ types/audit.ts
@@ -0,0 +1,45 @@
+// Page Status
+export interface PageStatus {
+    status: number;
+    statusText: string;
+}
+
+// OG
+export interface OG{
+    title?: string;
+    description?: string;
+    image?: string;
+}
+
+// Twitter
+export interface Twitter{
+    title?: string;
+    description?: string;
+    image?: string;
+}
+
+// Meta
+export interface Meta {
+    title?: string;
+    description?: string;
+    canonical?: string;
+    favicon?: string;
+    og: OG;
+    twitter: Twitter
+}
+
+export interface Warning{
+    message:string;
+    severity: 'error' | 'warning' | 'info';
+}
+
+// Top Level Audit Result
+export interface AuditResult {
+    id: string;
+    url: string;
+    pageStatus: PageStatus;
+    auditedAt: string;
+    cached: boolean;
+    warnings: Warning[];
+    meta: Meta
+}
